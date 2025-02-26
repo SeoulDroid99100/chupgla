3872fdeff5bf56693d7b2b04158d35fca9718293
SeoulDroid99100 <zekey8249+1@gmail.com>
2025-02-26 16:41:30 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,14 +2,14 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6765826972"
+    OWNER_ID = "6783092268"
     sudo_users = "6845325416", "6765826972"
     GROUP_ID = -1002133191051
-    TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
-    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
-    PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
+    TOKEN = "7227094800:AAHoegjcwXUFOgeJbLnxnwbKKeRjHT4Ba5A"
+    mongo_url = "mongodb+srv://Lundmate:sUF$e7pzitFXCGH@cluster0.vpqx1.mongodb.net/?retryWrites=true&w=majority"
+    PHOTO_URL = ["https://envs.sh/taC.jpg"]
     SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
+    UPDATE_CHAT = "Collect_em_support",
     BOT_USERNAME = "Collect_Em_AllBot"
     CHARA_CHANNEL_ID = "-1002133191051"
     api_id = 26626068
