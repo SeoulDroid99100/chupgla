3303901e91f93a93bdcfd88da8458c5deb6bfa21
SeoulDroid99100 <zekey8249+1@gmail.com>
2025-02-26 17:48:51 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -239,7 +239,6 @@ def main() -> None:
 
     application.add_handler(CommandHandler(["guess", "protecc", "collect", "grab", "hunt"], guess, block=False))
     application.add_handler(CommandHandler("fav", fav, block=False))
-    application.add_handler(CommandHandler("util", shivu.modules.util.util_cmd, block=False))
     application.add_handler(MessageHandler(filters.ALL, message_counter, block=False))
     application.run_polling(drop_pending_updates=True)
     
