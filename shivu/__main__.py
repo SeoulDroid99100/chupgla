636135e933c03928e1a71b8debceba3be01f510b
SeoulDroid99100 <zekey8249+1@gmail.com>
2025-02-26 21:50:53 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -13,7 +13,22 @@ from telegram.ext import CommandHandler, CallbackContext, MessageHandler, filter
 from shivu import collection, top_global_groups_collection, group_user_totals_collection, user_collection, user_totals_collection, shivuu
 from shivu import application, SUPPORT_CHAT, UPDATE_CHAT, db, LOGGER
 from shivu.modules import ALL_MODULES
+from flask import Flask, jsonify
+import threading
 import nest_asyncio
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({"status": "running", "message": "Bot is alive!"})
+
+@app.route('/random')
+def random_number():
+    return jsonify({"number": random.randint(1, 50)})
+
+def run_flask():
+    app.run(host="0.0.0.0", port=7860)
+
 nest_asyncio.apply()
 
 
@@ -245,6 +260,7 @@ def main() -> None:
     application.run_polling(drop_pending_updates=True)
     
 if __name__ == "__main__":
+    threading.Thread(target=run_flask, daemon=True).start() 
     shivuu.start()
     LOGGER.info("Bot started")
     main()
