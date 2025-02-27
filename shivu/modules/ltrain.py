054851bd28d918837e1e852ce858475e61289cd8
SeoulDroid99100 <zekey8249+1@gmail.com>
2025-02-28 00:05:25 +0530

    Update ltrain.py

--- shivu/modules/ltrain.py
+++ shivu/modules/ltrain.py
@@ -42,7 +42,7 @@ async def can_train(user_data: dict, training_type: str) -> tuple:
     
     return True, ""
 
-@shivuu.on_message(filters.command("ltrain" || "lgrow" || "grow"))
+@shivuu.on_message(filters.command(["ltrain" "lgrow" || "grow"]))
 async def training_menu(client: shivuu, message: Message):
     user_id = message.from_user.id
     user_data = await xy.find_one({"user_id": user_id})
