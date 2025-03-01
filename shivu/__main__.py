cba961e0b1886b6ba51693fa74c0741ad4db0b25
SeoulDroid99100 <zekey8249+1@gmail.com>
2025-03-01 17:44:16 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -3,7 +3,7 @@ import time
 import random
 import re
 import asyncio
-from html import escape 
+from html import escape
 
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton
@@ -13,10 +13,12 @@ from telegram.ext import CommandHandler, CallbackContext, MessageHandler, filter
 from shivu import collection, top_global_groups_collection, group_user_totals_collection, user_collection, user_totals_collection, shivuu
 from shivu import application, SUPPORT_CHAT, UPDATE_CHAT, db, LOGGER
 from shivu.modules import ALL_MODULES
-from shivu.modules.lrank import periodic_rank_updates, initialize_rank_db # Import 
+from shivu.modules.lrank import periodic_rank_updates, initialize_rank_db  # Import
+from shivu.modules.lloan import initialize_loan_db, periodic_loan_checks  # Import
 from flask import Flask, jsonify
 import threading
 import nest_asyncio
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -35,22 +37,17 @@ nest_asyncio.apply()
 for module_name in ALL_MODULES:
     imported_module = importlib.import_module("shivu.modules." + module_name)
 
-
-
 # Start task
-
-
 def main() -> None:
     """Run bot."""
     application.run_polling(drop_pending_updates=True)
-    initialize_rank_db()  # Call both initialization functions
-    initialize_loan_db()
+    initialize_rank_db()  # Call initialization functions
+    initialize_loan_db()  # Call initialization functions
     shivuu.loop.create_task(periodic_rank_updates())  # Start the tasks
-    shivuu.loop.create_task(periodic_loan_checks())
+    shivuu.loop.create_task(periodic_loan_checks())    # Start the tasks
 
 if __name__ == "__main__":
-    threading.Thread(target=run_flask, daemon=True).start() 
+    threading.Thread(target=run_flask, daemon=True).start()
     shivuu.start()
     LOGGER.info("Bot started")
     main()
-
