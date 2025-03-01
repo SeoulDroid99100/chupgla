0e78d7b20a5a4f1a6000ae0cbbb4acff6c326d48
SeoulDroid99100 <zekey8249+1@gmail.com>
2025-03-01 11:24:19 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -13,7 +13,7 @@ from telegram.ext import CommandHandler, CallbackContext, MessageHandler, filter
 from shivu import collection, top_global_groups_collection, group_user_totals_collection, user_collection, user_totals_collection, shivuu
 from shivu import application, SUPPORT_CHAT, UPDATE_CHAT, db, LOGGER
 from shivu.modules import ALL_MODULES
-from shivu.modules.lrank import periodic_rank_updates, initialize_db # Import 
+from shivu.modules.lrank import periodic_rank_updates, initialize_rank_db # Import 
 from flask import Flask, jsonify
 import threading
 import nest_asyncio
@@ -37,13 +37,17 @@ for module_name in ALL_MODULES:
 
 
 
-shivuu.loop.create_task(periodic_rank_updates()) # Start task
+# Start task
 
 
 def main() -> None:
     """Run bot."""
     application.run_polling(drop_pending_updates=True)
-    
+    await initialize_rank_db()  # Call both initialization functions
+    await initialize_loan_db()
+    shivuu.loop.create_task(periodic_rank_updates())  # Start the tasks
+    shivuu.loop.create_task(periodic_loan_checks())
+
 if __name__ == "__main__":
     threading.Thread(target=run_flask, daemon=True).start() 
     shivuu.start()
