13bc5203f55d8fa29f5a145a49c93f08dae2d310
SeoulDroid99100 <zekey8249+1@gmail.com>
2025-03-02 22:36:25 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -17,7 +17,8 @@ from shivu.modules.lrank import periodic_rank_updates, initialize_rank_db  # Imp
 from shivu.modules.lloan import initialize_loan_db, periodic_loan_checks  # Import
 from flask import Flask, jsonify
 import threading
-import nest_asyncio
+import uvloop
+uvloop.install()
 
 app = Flask(__name__)
 
