266c9c1b7c8fce3244e8d2d204b20105eae90127
SeoulDroid99100 <zekey8249+1@gmail.com>
2025-03-03 17:40:26 +0530

    Update __init__.py

--- shivu/modules/pvp/__init__.py
+++ shivu/modules/pvp/__init__.py
@@ -1,6 +1,7 @@
 #shivu.modules.pvp.__init__.py
 
 from . import editor
+from . import pvp
 # Optional: Add logging or initialization logic if needed
 def __init_module__():
     print("Pokémon Team Manager module loaded successfully!")
