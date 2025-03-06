71d06c8317a69ef56fac970650bb3dd250e4d1dc
SeoulDroid99100 <zekey8249+1@gmail.com>
2025-03-06 12:25:25 +0530

    Delete shivu/modules/pvp directory

--- shivu/modules/pvp/__init__.py
+++
@@ -1,7 +0,0 @@
-#shivu.modules.pvp.__init__.py
-
-from . import editor
-from . import pvp
-# Optional: Add logging or initialization logic if needed
-def __init_module__():
-    print("Pokémon Team Manager module loaded successfully!")
