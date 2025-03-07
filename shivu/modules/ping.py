1ad84ba415ba4ffb384112983ce02897cc7bed91
SeoulDroid99100 <zekey8249+1@gmail.com>
2025-03-07 21:24:01 +0530

    Update ping.py

--- shivu/modules/ping.py
+++ shivu/modules/ping.py
@@ -1,18 +1,16 @@
+from pyrogram import filters
 import time
 
-from telegram import Update
-from telegram.ext import CommandHandler, CallbackContext
+from shivu import shivuu as client, sudo_users
 
-from shivu import application, sudo_users
-
-async def ping(update: Update, context: CallbackContext) -> None:
-    if str(update.effective_user.id) not in sudo_users:
-        update.message.reply_text("Nouu.. its Sudo user's Command..")
+@client.on_message(filters.command("ping"))
+async def ping(_, message):
+    if str(message.from_user.id) not in sudo_users:
+        await message.reply("Nouu.. its Sudo user's Command..")
         return
-    start_time = time.time()
-    message = await update.message.reply_text('Pong!')
-    end_time = time.time()
-    elapsed_time = round((end_time - start_time) * 1000, 3)
-    await message.edit_text(f'Pong! {elapsed_time}ms')
 
-application.add_handler(CommandHandler("ping", ping))
+    start = time.time()
+    msg = await message.reply("Pong!")
+    end = time.time()
+    elapsed = round((end - start) * 1000, 3)
+    await msg.edit(f"Pong! {elapsed}ms")
